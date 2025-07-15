33a0bd3ad2d460cb1d9d197852d9590626967776
DarkXsOriginal <lev-zyryanov@mail.ru>
2025-07-15 16:25:31 +0800

    Добавление Download

---
+++ ImageboardBackuper/Download.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks;
+
+namespace ImageboardBackuper
+{
+    public static class Download
+    {
+        public static async Task DownloadFileAsync(string url, string filePath)
+        {
+            using (HttpClient client = new HttpClient()) // Создаем HttpClient
+            {
+                try
+                {
+                    // Отправляем GET-запрос и получаем ответ
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    // Убеждаемся, что запрос был успешным (код 200 OK)
+                    response.EnsureSuccessStatusCode();
+
+                    // Читаем содержимое ответа как поток
+                    using (Stream stream = await response.Content.ReadAsStreamAsync())
+                    {
+                        // Создаем файл и записываем в него содержимое потока
+                        using (FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None))
+                        {
+                            await stream.CopyToAsync(fileStream);
+                        }
+                    }
+
+                    Console.WriteLine($"Файл успешно загружен в: {filePath}");
+
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Ошибка при загрузке файла: {e.Message}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Произошла ошибка: {e.Message}");
+                }
+            }
+        }
+
+        public static async Task Main(string[] args)
+        {
+            string url = "https://i.pinimg.com/736x/92/52/26/92522636e62ff8685a72da760b871f51.jpg";
+            string filePath = @"C:\Users\Leon\Documents\PNG out";  // Замените на существующую папку
+
+            await DownloadFileAsync(url, filePath);
+        }
+    }
+}
+
--- TestProject2/UnitTest1.cs
+++ TestProject2/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Windows;
 using ImageboardBackuper;
 
